32a4577f15969ba35ca38d8221928be7dc4d7199
JoseRuiSilva <a106831@uminho.pt>
2024-11-08 12:42:42 +0000

    d) feito

--- jobscli.py
+++ jobscli.py
@@ -1,7 +1,14 @@
 from typing import Optional
+from typing import List
 import requests
 import typer
 from typing_extensions import Annotated
+import spacy
+from spacy.matcher import PhraseMatcher
+from skillNer.general_params import SKILL_DB
+from skillNer.skill_extractor_class import SkillExtractor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime
 
 app = typer.Typer()
 list_results = []
@@ -38,6 +45,47 @@ def fetch_data():
     response["results"] = list_results  # Finalmente cria o 'response' com todos os resultados
     print("Finalizado.")
 
+def filter_by_dates_results(list_results, start_date, end_date):
+    sorted_results = sorted(list_results, key=lambda x: x["updatedAt"], reverse=True)
+    filtered_results = []
+
+    for res in sorted_results:
+        update_date_str = res['updatedAt'][:10]
+        update_date = datetime.strptime(update_date_str, '%Y-%m-%d')
+        if start_date <= update_date <= end_date:
+            filtered_results.append(res)
+        elif update_date < start_date:
+            break
+
+    return filtered_results
+
+def process_job(res, given_skills, skill_extractor):
+    body = res['body']
+    try:
+        annotations = skill_extractor.annotate(body)
+    except (IndexError, ValueError) as e:
+        print(f"Erro a processar o 'body': {e}")
+        return None
+
+    anoted_skills = set(skill['doc_node_value'] for skill in annotations['results']['full_matches'])
+    anoted_skills.update(skill['doc_node_value'] for skill in annotations['results']['ngram_scored'])
+
+    if set(given_skills) & anoted_skills:   #PERGUNTAR AO PROFESSOR, se for todos as skills dadas all(item in annoted_skills for item in given_skills)
+        return res
+    return None
+
+def process_jobs_concurrently(list_of_results, given_skills, skill_extractor, max_workers=4):
+    results = []
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_res = {executor.submit(process_job, res, given_skills, skill_extractor): res for res in list_of_results}
+
+        for future in as_completed(future_to_res):
+            res = future.result()
+            if res:
+                results.append(res)
+
+    return results
+
 # Comando para obter os n trabalhos publicados mais recentes
 @app.command()
 #a)
@@ -48,17 +96,34 @@ def top(n: int):  # Chama o número de trabalhos a escolher n
     sorted_results = sorted(list_results, key=lambda x: x["publishedAt"], reverse=True)  # Ordena a lista de resultados pela data de publicação
     print(sorted_results[:n])  # Devolve os n primeiros valores da lista
 
-#b)
+#d)
 @app.command()
-def search(company_location:str, company_name:str, n_jobs:int):
+def skills(given_skills:List[str], start_date:str, end_date:str):
     if not list_results:
         fetch_data()
 
-#d)
-@app.command()
-def skills(skills:str, nome_empresa:str, num_trabalhos:int):
-    if not list_results:
-        fetch_data()
+    
+    # Iniciar o skill extractor
+    nlp = spacy.load("en_core_web_lg")
+    skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
+
+    try:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    except ValueError:
+        print("Erro ao ler as datas, verifique se estas existem.")
+        return {}
+    
+    if end_date < start_date:
+        print("Data final menor que inicial, a iniciar com valores trocados...")
+        filtered_results = filter_by_dates_results(list_results, end_date, start_date)
+    else:
+        filtered_results = filter_by_dates_results(list_results, start_date, end_date)
+
+    results = process_jobs_concurrently(filtered_results, given_skills, skill_extractor, max_workers=8)
+    print(results)
+
+    
 
 if __name__ == "__main__":
     app()  # Executa a app Typer
