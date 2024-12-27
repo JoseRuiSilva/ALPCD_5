1ba040e18a038540892b9bf39c247159178f090e
JoseRuiSilva <a106831@uminho.pt>
2024-12-27 13:51:53 +0000

    Adição do c) e e), tal como alteração da função de request

--- jobscli.py
+++ jobscli.py
@@ -16,13 +16,8 @@ app = typer.Typer()
 list_results = []
 
 # Função para aceder ao URL
-def pedido(limit, page, job_id = None):
-    if job_id:
-        url = f"https://api.itjobs.pt/job/get.json?api_key=ee176fa9456283ab9c42f357b036e236&id={job_id}"
-    else:
-        url = f"https://api.itjobs.pt/job/list.json?api_key=ee176fa9456283ab9c42f357b036e236&limit={limit}&page={page}"
+def request(url, headers):
     payload = {}
-    headers = {'User-Agent': "ALPCD_5", 'Cookie': 'itjobs_pt=3cea3cc1f4c6a847f8c459367edf7143:94de45f2a55a15b2672adf8788ac8072e7bfd5c5'}  # Necessário por 'User-Agent' nos headers
     res = requests.request("GET", url, headers=headers, data=payload)
     if res.status_code == 200:  # Verificar se a resposta foi bem-sucedida (200 OK)
         results = res.json()
@@ -55,7 +50,9 @@ def fetch_data():
     global list_results  # Para ser acessado dentro de outras funções
     limit = 100
     page = 1
-    response = pedido(limit, page)
+    url = f"https://api.itjobs.pt/job/list.json?api_key=ee176fa9456283ab9c42f357b036e236&limit={limit}&page={page}"
+    headers = {'User-Agent': "ALPCD_5", 'Cookie': 'itjobs_pt=3cea3cc1f4c6a847f8c459367edf7143:94de45f2a55a15b2672adf8788ac8072e7bfd5c5'}
+    response = request(url, headers)
 
     # Verifica se a resposta contém a chave 'results' ou se não há resposta
     if not response or "results" not in response:
@@ -68,7 +65,8 @@ def fetch_data():
     while page * limit < response["total"]:  # Para limite=100 e total=1261, página vai até 13
         page += 1
         try:  # Caso seja devolvido um dicionário vazio, para não ocorrerem erros
-            new_results = pedido(limit, page)["results"]
+            url = f"https://api.itjobs.pt/job/list.json?api_key=ee176fa9456283ab9c42f357b036e236&limit={limit}&page={page}"
+            new_results = request(url, headers)["results"]
             list_results += new_results  # Lista com todos os resultados que vai incrementando
         except:
             print(f"Erro ao obter resultados da página {page}.")
@@ -194,7 +192,9 @@ def salary(job_id: Annotated[str, typer.Argument(help="Id do trabalho")]):
     Obtém o salário dum trabalho.
     """
     # Usar a função com retentativa para obter os detalhes do job
-    job_data = pedido(100,1,job_id)
+    url = f"https://api.itjobs.pt/job/get.json?api_key=ee176fa9456283ab9c42f357b036e236&id={job_id}"
+    headers = {'User-Agent': "ALPCD_5", 'Cookie': 'itjobs_pt=3cea3cc1f4c6a847f8c459367edf7143:94de45f2a55a15b2672adf8788ac8072e7bfd5c5'}  # Necessário por 'User-Agent' nos headers
+    job_data = request(url, headers)
     
     if job_data is None:
         print("Não foi possível obter os dados do job. Verifique o job_id e tente novamente.")
@@ -260,6 +260,15 @@ def skills(given_skills:Annotated[List[str], typer.Argument(help="Competências 
         export_to_csv(results, "skills.csv")
         print("Dados exportados para top_jobs.csv")
     
+#TP2
+#c)
+
+
+#e)
+
+
+
+
 
 if __name__ == "__main__":
     app()  # Executa a app Typer
